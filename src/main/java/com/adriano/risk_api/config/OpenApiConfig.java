bb351c6d93f652581209283abd951c8533293f08
adrianoreck <adrianoreck@gmail.com>
2026-08-05 21:41:48 -0400

    feat: Update the OpenApiConfig to allow the authorization (JWT) on the interface for easy testing

--- src/main/java/com/adriano/risk_api/config/OpenApiConfig.java
+++ src/main/java/com/adriano/risk_api/config/OpenApiConfig.java
@@ -1,17 +1,31 @@
 package com.adriano.risk_api.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import io.swagger.v3.oas.annotations.Operation;
 
 @Configuration
 public class OpenApiConfig {
 
+    private static final String BEARER_SCHEME = "bearerAuth";
+
     @Bean
     public OpenAPI riskApiOpenAPI() {
         return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(
+                                BEARER_SCHEME,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(BEARER_SCHEME))
                 .info(new Info()
                         .title("Risk Assessment API")
                         .description("API for customer risk assessment and scoring")
